87a81ab19c8e6147302bc43357aa40e4f8231ea2
animeing <msdsoftpopo@yahoo.co.jp>
2022-05-03 05:30:16 +0900

    音源データをDBに登録するコード追加

--- parts/db/dao/AlbumDao.php
+++ parts/db/dao/AlbumDao.php
@@ -4,4 +4,25 @@ class AlbumDao extends SqlCreater implements AlbumTable{
     function createDto() {
         return new AlbumDto();
     }
+
+    /**
+     * @return AlbumDto[]
+     */
+    function findAlbumIdAndArtistId($albumTitle, $artistId) {
+        return $this->toDtoList(
+            $this->execute(
+                $this->whereQuery(AlbumTable::TITLE.$this::EQUAL_AND.AlbumTable::ARTIST_ID.$this::EQUAL_PARAM),
+                array($albumTitle, $artistId)
+            )
+        );
+    }
+
+    function hasRegistedAlbum($albumTitle, $artistName) {
+        foreach($this->findAlbumIdAndArtistId($albumTitle, sha1($artistName)) as $albumDto) {
+            if($albumDto->getAlbumKey() == sha1($albumTitle).sha1($artistName)) {
+                return $albumDto;
+            }
+        }
+        return false;
+    }
 }
