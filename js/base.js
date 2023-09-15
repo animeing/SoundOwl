91654723ef470090b3c226cc52beb86748ed0924
animeing <msdsoftpopo@yahoo.co.jp>
2023-09-16 00:10:57 +0900

    Update titleにページ名、音源タイトルを表示

--- js/base.js
+++ js/base.js
@@ -2723,6 +2723,7 @@ class AudioPlayer{
                     this.audioDeployment();
                 }
             }
+            setTitle(audioClip.title);
             this.audio.play();
             this.currentPlayState = AudioPlayStateEnum.PLAY;
             this.eventSupport.dispatchEvent(new CustomEvent('play'));
