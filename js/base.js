d6d3df9f4492118f25b002b0984d030cd73a6565
animeing <msdsoftpopo@yahoo.co.jp>
2023-11-12 18:26:42 +0900

    Update EqualizerのQuality値を一律1から対数スケールに合うように変更

--- js/base.js
+++ js/base.js
@@ -2911,6 +2911,7 @@ class AudioEqualizer {
      */
     constructor(audioSource, audioContext) {
         this._gainFlat = [{'hz':8,'gain':0},{'hz':16,'gain':0},{'hz':32,'gain':0},{'hz':64,'gain':0},{'hz':125,'gain':0},{'hz':250,'gain':0},{'hz':500,'gain':0},{'hz':1e3,'gain':0},{'hz':2e3,'gain':0},{'hz':4e3,'gain':0},{'hz':8e3,'gain':0},{'hz':16e3,'gain':0},{'hz':24e3,'gain':0}];
+        const QUALITY_FACTOR = {8:{'q':0.7},16:{'q':0.7},32:{'q':0.7},64:{'q':1.0},125:{'q':1.1},250:{'q':1.1},500:{'q':1.1},1e3:{'q':1.2},2e3:{'q':1.3},4e3:{'q':1.4},8e3:{'q':1.5},16e3:{'q':1.7},24e3:{'q':1.8}};
         this._currentGains =  JSON.parse(JSON.stringify(this._gainFlat));
         /**
          * @type {BiquadFilterNode[]}
@@ -2924,9 +2925,9 @@ class AudioEqualizer {
                 filter.type = 'highshelf';
             } else {
                 filter.type = 'peaking';
-                filter.Q.value = 1;
             }
             filter.frequency.value = element.hz;
+            filter.Q.value = QUALITY_FACTOR[element.hz].q;
             filter.gain.value = element.gain;
             this.filters[element.hz] = filter;
         });
