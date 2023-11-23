ffc3c29c25978d85d2baa653edc8e95a423272f3
animeing <msdsoftpopo@yahoo.co.jp>
2023-11-23 10:37:43 +0900

    Fix 誤って消したファイルの追加

---
+++ frontend/src/audio/type/AudioClip.js
@@ -0,0 +1,41 @@
+import { BASE } from "../../utilization/path";
+
+export class AudioClip{
+    constructor(){
+        this.no=0;
+        this.soundHash=null;
+        this.artist=null;
+        this.title=null;
+        this.album=null;
+        this.albumKey = null;
+    }
+
+    get src(){
+        if(this.soundHash != null){
+            return BASE.HOME+'sound_create/sound.php?media_hash='+this.soundHash;
+        }
+        return null;
+    }
+
+    /**
+     * @param {AudioClip} param
+     */
+    equals(param){
+        if(param == null || param.no != this.no){
+            return false;
+        }
+        if(param.soundHash != this.soundHash){
+            return false;
+        }
+        return true;
+    }
+    static createAudioClip(serverFileTitle, title, album, artist, no){
+        let audioClip = new AudioClip;
+        audioClip.no = no;
+        audioClip.title = title;
+        audioClip.album = album;
+        audioClip.soundHash = serverFileTitle;
+        audioClip.artist = artist;
+        return audioClip;
+    }
+}
