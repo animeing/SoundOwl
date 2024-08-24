c0d2c896780b11207eae8d27e72d282b9465dce5
animeing <msdsoftpopo@yahoo.co.jp>
2024-08-24 14:58:45 +0900

    Fix AlbumArtのインターレース表示取りやめ、RedisにてAlbumArtのCache実施

--- img/album_art.php
+++ img/album_art.php
@@ -5,7 +5,6 @@ error_reporting(E_ALL & ~E_WARNING);
 require(dirname(__DIR__).'/parts/loader.php');
 require(dirname(__DIR__).'/vendor/autoload.php');
 
-
 header('Cache-Control: public');
 header('Pragma: public');
 
@@ -17,18 +16,34 @@ if(!isset($_GET["media_hash"])) {
     return;
 }
 
-
 $decrypted = ComplessUtil::decompless((urldecode($_GET["media_hash"])));
-$albumDao = new AlbumDao;
-$albumDto = null;
-foreach($albumDao->find($decrypted) as $findAlbumDto) {
-    if($findAlbumDto->getAlbumKey() == $decrypted) {
-        $albumDto = $findAlbumDto;
-        break;
+
+$predis = new Predis\Client([
+    'host'=>REDIS_SERVER
+]);
+$predis->executeRaw(['CONFIG', 'SET', 'maxmemory', '256mb']);
+$predis->executeRaw(['CONFIG', 'SET', 'maxmemory-policy', 'volatile-ttl']);
+
+$albumDto = new AlbumDto;
+
+if($predis->exists($decrypted)) {
+    $albumDto->putAllDtoCache(unserialize($predis->get($decrypted)));
+    header("X-Cache-Load: True");
+} else {
+    $albumDao = new AlbumDao;
+    foreach($albumDao->find($decrypted) as $findAlbumDto) {
+        if($findAlbumDto->getAlbumKey() == $decrypted) {
+            $albumDto = $findAlbumDto;
+            break;
+        }
     }
+    header("X-Cache-Load: False");
+    $predis->setex($decrypted, 3600, serialize($albumDto->getDtoCache()));
 }
 
-if($albumDto == null || $albumDto->getAlbumArt() == null) {
+
+
+if($albumDto->getAlbumArt() == null) {
     $mime = mime_content_type(dirname(__DIR__).'/img/blank-image.png');
     header("Accept: {$mime}");
     header("Content-type: {$mime}");
@@ -36,31 +51,12 @@ if($albumDto == null || $albumDto->getAlbumArt() == null) {
     return;
 }
 
-header("Accept-Ranges: bytes"); 
+header("Accept-Ranges: bytes");
 $mime = $albumDto->getArtMime();
 header("Accept: {$mime}");
 header("Content-type: {$mime}");
-if(strpos($mime,'jpeg') !== false) {
-    if(file_put_contents(urldecode($_GET["media_hash"]).'.jpg', $albumDto->getAlbumArt()) === false){
-        echo $albumDto->getAlbumArt();
-        return;
-    }
-    $imageResource = imagecreatefromjpeg(urldecode($_GET["media_hash"]).'.jpg');
-    imageinterlace($imageResource, true);
-    imagejpeg($imageResource, null, 70);
-    unlink(urldecode($_GET["media_hash"]).'.jpg');
-    imagedestroy($imageResource);
-} else if(strpos($mime,'png') !== false){
-    if(file_put_contents(urldecode($_GET["media_hash"]).'.png', $albumDto->getAlbumArt()) === false) {
-        echo $albumDto->getAlbumArt();
-        return;
-    }
-    $imageResource = imagecreatefrompng(urldecode($_GET["media_hash"]).'.png');
-    imageinterlace($imageResource, true);
-    imagepng($imageResource);
-    unlink(urldecode($_GET["media_hash"]).'.png');
-    imagedestroy($imageResource);
 
-} else {
-    echo $albumDto->getAlbumArt();
-}
+$imageData = $albumDto->getAlbumArt();
+
+echo $imageData;
+
