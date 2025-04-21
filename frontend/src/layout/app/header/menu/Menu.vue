a66407e0db0ddda9d403e45c4758acf8a57b58bf
animeing <msdsoftpopo@yahoo.co.jp>
2025-04-21 23:28:41 +0900

    Fix スマホでメニューが開かない問題修正

--- frontend/src/layout/app/header/menu/Menu.vue
+++ frontend/src/layout/app/header/menu/Menu.vue
@@ -1,16 +1,20 @@
 <template>
   <v-menu
-    open-on-hover 
+    :open-on-hover="isDesktop"
+    :close-on-content-click="false"
     :max-width="200"
     transition="scale-transition"
-    :close-on-content-click="false"
-    >
+  >
     <template #activator="{ props }">
       <v-btn
         v-bind="props"
         width="5%"
-        variant="elevated">Menu</v-btn>
+        variant="elevated"
+      >
+        Menu
+      </v-btn>
     </template>
+
     <v-list>
       <v-list-item
         v-for="(item, index) in menuItems"
@@ -24,35 +28,24 @@
   </v-menu>
 </template>
 
-
 <script>
-  export default {
-    data() {
-      return {
-        menuItems: [
-          {
-            title: 'Artist List',
-            route: { name: 'artist_list' },
-            icon: 'mdi-account-music',
-          },
-          {
-            title: 'Album List',
-            route: { name: 'album_list' },
-            icon: 'mdi-album',
-          },
-          {
-            title: 'Play List',
-            route: { name: 'playlists' },
-            icon: 'mdi-playlist-music',
-          },
-          {
-            title: 'History List',
-            route: { name: 'history_list' },
-            icon: 'mdi-history',
-          },
-          { title: 'Setting', route: { name: 'setting' }, icon: 'mdi-cog' },
-        ],
-      }
-    },
+export default {
+  data() {
+    return {
+      menuItems: [
+        { title: 'Artist List',  route: { name: 'artist_list' },  icon: 'mdi-account-music' },
+        { title: 'Album List',   route: { name: 'album_list' },   icon: 'mdi-album' },
+        { title: 'Play List',    route: { name: 'playlists'   },  icon: 'mdi-playlist-music' },
+        { title: 'History List', route: { name: 'history_list'},  icon: 'mdi-history' },
+        { title: 'Setting',      route: { name: 'setting'     },  icon: 'mdi-cog' }
+      ]
+    }
+  },
+  computed: {
+    // md 未満＝スマホ扱い。PC(マウス)だけ hover を有効化
+    isDesktop() {
+      return this.$vuetify.display.mdAndUp
+    }
   }
+}
 </script>
