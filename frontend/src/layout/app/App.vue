4686f0096cd937f74bcef67392bb8eca1d96d64b
animeing <msdsoftpopo@yahoo.co.jp>
2025-04-30 15:11:53 +0900

    Fix FooterControllerがMainコンテンツを隠してしまっている問題を修正

--- frontend/src/layout/app/App.vue
+++ frontend/src/layout/app/App.vue
@@ -1,11 +1,12 @@
 <template>
   <v-app>
     <Header></Header>
-    <v-main id="base" class="layout-base">
+    <v-main id="base" class="layout-base" :style="{ marginTop: '5px', paddingBottom: `${controllerHeight}px` }">
+      <!-- AudioControllerの高さを考慮してpadding-bottomを動的に設定 -->
       <router-view></router-view>
     </v-main>
-    <div id="controller" class="audio-play-controller analyser">
-      <AudioController ></AudioController>
+    <div id="controller" class="audio-play-controller analyser" ref="controller">
+      <AudioController></AudioController>
     </div>
   </v-app>
 </template>
@@ -13,25 +14,38 @@
 import AudioController from '../footer/AudioController.vue';
 import Header from './header/Header.vue';
 import colorThema from '../../thema';
-  
+
 export default {
   name: 'App',
-  components:{
+  components: {
     AudioController,
     Header
   },
-  data () {
+  data() {
     return {
       style: {
         '--background-color': '#eee'
-      }
-    }
+      },
+      controllerHeight: 0 // AudioControllerの高さを保持
+    };
   },
-
   mounted() {
     this.style['--background-color'] = colorThema.backgrouund;
+    this.updateControllerHeight();
+    window.addEventListener('resize', this.updateControllerHeight);
   },
-}
+  beforeDestroy() {
+    window.removeEventListener('resize', this.updateControllerHeight);
+  },
+  methods: {
+    updateControllerHeight() {
+      const controller = this.$refs.controller;
+      if (controller) {
+        this.controllerHeight = controller.offsetHeight;
+      }
+    }
+  }
+};
 </script>
 <style lang="scss">
 
