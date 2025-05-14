86a8c779e1eeb8d63b88cc5f97b806aea675aa08
animeing <msdsoftpopo@yahoo.co.jp>
2025-05-15 03:36:25 +0900

    Update アルバムアートを表示できるように更新

--- frontend/src/layout/footer/parts/AudioCanvas.vue
+++ frontend/src/layout/footer/parts/AudioCanvas.vue
@@ -4,17 +4,29 @@
     @select="(item) => item.action()"
     style="height: 100%;width: 100%;"
   >
+    <transition-group
+      name="album-art"
+      tag="div"
+      class="album-art-container"
+    >
+      <img
+        v-for="art in arts"
+        :key="art.id"
+        :src="art.url"
+        class="album-art-img"
+      />
+    </transition-group>
+
     <canvas
       ref="canvas"
       v-if="isVisibleAnalyser"
       class="analyser-view"
       style="height: 100%;display: block;"
       :width="width"
-      :height="height"
-    />
+      :height="height"></canvas>
 
     <div
-      v-else
+      v-if="isLyricsVisible"
       class="lyrics-view"
     >
       <pre>{{ lyrics }}</pre>
@@ -27,6 +39,7 @@ import audio from '../../../audio/AudioPlayer';
 import { AudioPlayStateEnum } from '../../../audio/enum/AudioPlayStateEnum';
 import { BaseFrameWork } from '../../../base';
 import ContextMenu from '../../common/ContextMenu.vue';
+import { BASE } from '../../../utilization/path';
 
 export default {
   name: 'AudioCanvas',
@@ -42,10 +55,35 @@ export default {
       width: window.innerWidth,
       height: window.innerHeight - 45,
       isVisibleAnalyser: true,
+      isLyricsVisible: false,
       lyrics: '',
+      arts: [],
+      seq: 0,
       menuItems: [
-        { label: 'Lyrics', action: () => { this.isVisibleAnalyser = false; this.$emit('toggleView'); this.resetCanvas(); } },
-        { label: 'Visualizer', action: () => { this.isVisibleAnalyser = true; this.$emit('toggleView'); this.resetCanvas(); } }
+        { label: 'Lyrics',
+          action: () => {
+            this.isVisibleAnalyser = false;
+            this.isLyricsVisible = true;
+            this.$emit('toggleView');
+            this.resetCanvas();
+          }
+        },
+        { label: 'Visualizer',
+          action: () => {
+            this.isVisibleAnalyser = true;
+            this.isLyricsVisible = false;
+            this.$emit('toggleView');
+            this.resetCanvas();
+          }
+        },
+        { label: 'Album Art',
+          action: () => {
+            this.isVisibleAnalyser = false;
+            this.isLyricsVisible = false;
+            this.$emit('toggleView');
+            this.resetCanvas();
+          }
+        },
       ]
     };
   },
@@ -53,12 +91,15 @@ export default {
     this.initializeCanvas();
     this.updateLyrics();
     audio.eventSupport.addEventListener('audio_info_loaded', this.updateLyrics);
+    audio.eventSupport.addEventListener('audio_info_loaded', this.pushArt);
+    this.pushArt();
   },
   beforeUnmount() {
     if (this.animationId) {
       cancelAnimationFrame(this.animationId);
     }
     audio.eventSupport.removeEventListener('audio_info_loaded', this.updateLyrics);
+    audio.eventSupport.removeEventListener('audio_info_loaded', this.pushArt);
   },
   methods: {
     initializeCanvas() {
@@ -70,6 +111,14 @@ export default {
       } else {
         console.error('Canvas element not found or getContext is not a function');
       }
+    },
+    pushArt(){
+      if(audio.currentAudioClip == null) return; // 現在の曲が無い場合は何もしない
+      const url = `${BASE.HOME}img/album_art.php?media_hash=${audio.currentAudioClip.albumKey}`;
+      if(this.seq[this.seq.length-1] == url) return; // 連続で同じアートを追加しない
+      this.arts.push({id:++this.seq,url});
+      // 600ms 後に旧アートを削除して1枚だけ残す
+      setTimeout(()=>{ if(this.arts.length>1) this.arts.shift(); }, 250);
     },
     resetCanvas() {
       if (this.isVisibleAnalyser) {
@@ -154,5 +203,33 @@ canvas {
   overflow-y: auto;
   height: 100%;
   display: block; /* Ensure the lyrics-view is visible */
+}
+.album-art-container{
+  position:absolute; inset:0;
+  overflow:hidden; perspective:1000px;
+}
+.album-art-img{
+  position:absolute; top:50%; left:50%;
+  width:60vmin; height:60vmin; object-fit:cover;
+  transform:translate(-50%,-50%);
+}
+
+/* === トランジション === */
+.album-art-enter-from{
+  transform:translate(-50%,-50%) translateZ(-500px) scale(.3);
+  opacity:0;
+}
+.album-art-enter-to,
+.album-art-leave-from{
+  transform:translate(-50%,-50%) translateZ(0) scale(1);
+  opacity:1;
+}
+.album-art-leave-to{
+  transform:translate(-50%,-50%) translateZ(-500px) scale(.3);
+  opacity:0;
+}
+.album-art-enter-active,
+.album-art-leave-active{
+  transition:transform .6s ease, opacity .6s ease;
 }
 </style>
