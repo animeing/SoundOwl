f75f8c2300268110c5858951003414a6fed1ec3e
animeing <msdsoftpopo@yahoo.co.jp>
2025-07-25 00:11:00 +0900

    Add Express frontend server with API env

--- frontend/src/utilization/path.js
+++ frontend/src/utilization/path.js
@@ -3,6 +3,6 @@ export const BASE = {
   SAVE_PATH:window.location.href.split('#')[0],
   HOME:window.location.href.split('#')[0],
   VUE_HOME:window.location.href.split('#')[0]+'#/',
-  API:window.location.href.split('#')[0]+'api/',
+  API:process.env.API_BASE_URL || window.location.href.split('#')[0]+'api/',
   WEBSOCKET:window.location.hostname
 };
